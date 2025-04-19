d15a7a789b0d217f2cf6feabba4d6f834797469f
LeF1nn <k8662232@gmail.com>
2025-04-19 23:22:06 +0330

    bankacc class

---
+++ 7.SimpleBankingSystem/BankAcc.java
@@ -0,0 +1,31 @@
+public class BankAcc {
+
+    private int balance;
+
+    public BankAcc() {
+        this.balance = (int) 0.0;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: $" + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
+
